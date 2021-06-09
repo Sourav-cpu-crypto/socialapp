3d9f1a8ea054f50b2efcddd7c402b0fd4a18b716
Sourav-cpu-crypto <souravmukherjeemca1@gmail.com>
2021-06-09 21:57:33 +0530

    push

--- index.php
+++ index.php
@@ -1,4 +1,29 @@
+<html>
+<head>
+</head>
+<body>
 <?php
 session_start();
-echo $_SESSION['email'];
+$email=$_SESSION['email'];
+$con = mysqli_connect("localhost", "root", "", "socialapp");  
+
+$sql ="select * from users where email='$email'";
+
+$res = mysqli_query($con,$sql);
+
+$row = mysqli_fetch_assoc($res);
+$name = $row['filename'];
+
+
+echo'<h1>"'.$name.'"</h1>';
+
+        
+        echo '<img width="600" height="600" src=images/'.$name.' '; 
+
+
+
+
+
+
 ?>
+</body>
